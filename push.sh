d741bad428350c81cac4919d4eb8c557353a362b
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-05 14:06:16 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Refactor . Refactor to create Generic Class to take in 3 variables of Generic Type"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
