78e46dd5c84bd402cd774e261a086298ec8adb63
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-05 12:17:40 +0530

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Given 3 Integers find the maximum number"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
